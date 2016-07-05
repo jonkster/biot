37af2cbeb0b6f08aaa1b5e565553fa8cd3987f12
Jonathan Kelly <jonk@bigpond.com>
2016-07-05 17:00:14 +1000

    Tidy up router application.

--- biot-applications/router/main.c
+++ biot-applications/router/main.c
@@ -1,28 +1,26 @@
 /*
- * Copyright (C) 2015 Cenk Gündoğan <cnkgndgn@gmail.com>
+ * Copyright (C) 2016 Jon Kelly
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
  * directory for more details.
  */
 
-/**
- * @brief       Introductory example for the IEEE WF-IOT 2015 Tutorial
- * @author      Cenk Gündoğan <cnkgndgn@gmail.com>
- */
 
 #include <stdio.h>
-#include "shell.h"
+#include <thread.h>
+#include <xtimer.h>
+
 #include "msg.h"
-#include <stdbool.h>
-#include <string.h>
-#include "board.h"
-#include "thread.h"
-#include "periph/gpio.h"
-#include "net/gnrc/ipv6/nc.h"
+#include "net/ipv6/addr.h"
+#include "net/gnrc/netif.h"
 #include "net/gnrc/ipv6/netif.h"
+#include "net/gnrc/rpl/dodag.h"
+#include "shell.h"
+#include "../modules/ssd1306/ssd1306.h"
 #include "../modules/biotUdp/udp.h"
 #include "../modules/identify/biotIdentify.h"
+#include "../modules/sendData/sendData.h"
 
 #if (defined NOOLED)
 #pragma message "Assuming no OLED available therefore samr21-xpro board"
@@ -38,20 +36,24 @@
 #endif
 
 
-#define PRIO    (THREAD_PRIORITY_MAIN - 1)
-#define Q_SZ    (8)
-static msg_t msg_q[Q_SZ];
-bool led_status = false;
-static char udp_stack[THREAD_STACKSIZE_DEFAULT];
+#define PRIO    (THREAD_PRIORITY_MAIN + 1)
 static char housekeeping_stack[THREAD_STACKSIZE_DEFAULT];
+#if !defined NOOLED
+static char display_stack[THREAD_STACKSIZE_DEFAULT];
+#endif
+static char udp_stack[THREAD_STACKSIZE_DEFAULT];
 
-static const shell_command_t shell_commands[];
+char dodagRoot[] = "affe::2";
 
-
-/* Add the shell command function here ###################################### */
+extern void batch(const shell_command_t *command_list, char *line);
 extern int udp_send(char *addr_str, char *data);
 extern uint32_t getCurrentTime(void);
+extern bool isTimeSet(void);
+bool hasTimeChanged(void);
+extern void timeInit(void);
+extern void sendData(char *address, nodeData_t data);
 extern void setCurrentTime(uint32_t t);
+/* ########################################################################## */
 
 void sync(void)
 {
@@ -79,49 +81,13 @@ int resetTime_cmd(int argc, char **argv)
     return 0;
 }
 
-int led_control(int argc, char **argv)
+int identify_cmd(int argc, char **argv)
 {
-    if (argc == 2) {
-        if (strcmp(argv[1], "on") == 0) {
-            led_status = true;
-            LED0_ON;
-            LED1_OFF;
-            return 0;
-        }
-        else if (strcmp(argv[1], "off") == 0) {
-            led_status = false;
-            LED0_OFF;
-            LED1_ON;
-            LED_RGB_OFF;
-            return 0;
-        }
-        else if (strcmp(argv[1], "red") == 0) {
-            led_status = false;
-            LED0_ON;
-            LED_RGB_OFF;
-            LED_RGB_R_ON;
-            return 0;
-        }
-        else if (strcmp(argv[1], "green") == 0) {
-            led_status = false;
-            LED0_ON;
-            LED_RGB_OFF;
-            LED_RGB_G_ON;
-            return 0;
-        }
-        else if (strcmp(argv[1], "blue") == 0) {
-            led_status = false;
-            LED0_ON;
-            LED_RGB_OFF;
-            LED_RGB_B_ON;
-            return 0;
-        }
-    }
-
-    return -1;
+    identifyYourself();
+    return 0;
 }
 
-extern void batch(const shell_command_t *command_list, char *line);
+
 
 bool isRoot = false;
 bool isRootPending = false;
@@ -138,12 +104,11 @@ void btnCallback(void* arg)
 }
 
 
-/* ########################################################################## */
+
+
 static const shell_command_t shell_commands[] = {
-
-/* Add a new shell command here ############################################# */
-
-    { "led", "use 'led on' to turn the LED on and 'led off' to turn the LED off", led_control },
+    /* Add a new shell commands here */
+    { "identify", "visually identify board", identify_cmd },
 
     { "sync", "synchronise time across nodes", sync_cmd },
 
@@ -153,7 +118,6 @@ static const shell_command_t shell_commands[] = {
 
     { "udp", "send a message: udp <IPv6-address> <message>", udp_cmd },
 
-    /* ########################################################################## */
     { NULL, NULL, NULL }
 };
 
@@ -162,10 +126,12 @@ void setRoot(void)
 {
     isRootPending = false;
     // set up rpl root
+    puts("setting as root node");
     batch(shell_commands, "ifconfig 6 add affe::2");
     batch(shell_commands, "rpl root 1 affe::2");
 
     // add wired interface
+    puts("setting as border router");
     batch(shell_commands, "ifconfig 7 add affe::3");
     // assume outside world is at affe::1
     batch(shell_commands, "ncache add 7 affe::1");
@@ -173,47 +139,73 @@ void setRoot(void)
     LED0_ON;
 }
 
+
 /* set interval to 1 second */
 #define INTERVAL (1000000U)
 void *housekeeping_handler(void *arg)
 {
-   int factor = 1; 
+    int counter = 0;
+    uint16_t lastSecs = 0;
     while(1)
     {
-        if (isRootPending)
+        uint16_t tsecs = getCurrentTime()/1500000;
+        if (tsecs != lastSecs)
         {
-            setRoot();
-            factor = 5;
+            if (isRootPending)
+            {
+                setRoot();
+            }
+
+            if (isRoot && counter++ > 10)
+            {
+                sync();
+                counter = 0;
+            }
+
+            if (tsecs % 2 == 0)
+            {
+                LED0_ON;
+                thread_yield();
+            }
+            else
+            {
+                LED0_OFF;
+                thread_yield();
+            }
+#if !defined NOOLED
+            char st[12];
+            sprintf(st, "T=%u", tsecs);
+            oledPrint(2, st);
+#endif
         }
-        uint32_t last_wakeup = xtimer_now();
-        xtimer_usleep_until(&last_wakeup, INTERVAL/(2*factor));
-        LED0_OFF;
-        xtimer_usleep_until(&last_wakeup, INTERVAL/factor);
-        LED0_ON;
+        lastSecs = tsecs;
+        thread_yield();
     }
 }
 
-
 int main(void)
 {
-    msg_init_queue(msg_q, Q_SZ);
-
-    puts("Type 'help' for a list of available commands");
-
+    puts("Biotz Root Node\n");
     LED0_OFF;
-    LED1_ON;
-    LED_RGB_OFF;
 
-    printf("Biotz Border Router\n");
-    batch(shell_commands, "rpl init 7");
-    gpio_init_int(BUTTON_GPIO, GPIO_IN_PU, GPIO_RISING, (gpio_cb_t)btnCallback, NULL);
+
+#if !defined NOOLED
+    thread_create(display_stack, sizeof(display_stack), PRIO, THREAD_CREATE_STACKTEST, (thread_task_func_t) display_handler,
+                  NULL, "display");
+#endif
+
+    thread_create(housekeeping_stack, sizeof(housekeeping_stack), PRIO, THREAD_CREATE_STACKTEST, housekeeping_handler,
+                  NULL, "housekeeping");
 
     thread_create(udp_stack, sizeof(udp_stack), PRIO, THREAD_CREATE_STACKTEST, udp_server,
                   NULL, "udp");
 
-    thread_create(housekeeping_stack, sizeof(housekeeping_stack), THREAD_PRIORITY_MAIN - 3, THREAD_CREATE_STACKTEST, housekeeping_handler,
-                  NULL, "housekeeping");
+    batch(shell_commands, "rpl init 6");
+    gpio_init_int(BUTTON_GPIO, GPIO_IN_PU, GPIO_RISING, (gpio_cb_t)btnCallback, NULL);
 
+    identifyYourself();
+
+    timeInit();
 
     char line_buf[SHELL_DEFAULT_BUFSIZE];
     shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
