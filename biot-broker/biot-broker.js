9016187d347b64349d716e1afe4a43d980b371a0
Jonathan Kelly <jonk@bigpond.com>
2016-07-22 10:52:39 +1000

    Added calibration type requests to web service.

--- biot-broker/biot-broker.js
+++ biot-broker/biot-broker.js
@@ -15,14 +15,26 @@ var dgram = require('dgram');
 // send message to Biotz Router device
 function sendBiotzRouterMessage() {
     var message = new Buffer('get-data');
-    var client = dgram.createSocket('udp6');
+    var client1 = dgram.createSocket('udp6');
 
-    client.send(message, 0, message.length, BIOTZ_UDP_PORT, BIOTZ_ROUTER_HOST, function(err, bytes) {
+    // ask for update on node data knowledge
+    client1.send(message, 0, message.length, BIOTZ_UDP_PORT, BIOTZ_ROUTER_HOST, function(err, bytes) {
         if (err)
         {
             console.log('Error:', err);
         }
-        client.close();
+        client1.close();
+    });
+
+    // ask for update on node calibration knowledge
+    message = new Buffer('get-cal');
+    client2 = dgram.createSocket('udp6');
+    client2.send(message, 0, message.length, BIOTZ_UDP_PORT, BIOTZ_ROUTER_HOST, function(err, bytes) {
+        if (err)
+        {
+            console.log('Error:', err);
+        }
+        client2.close();
     });
 }
 
@@ -37,11 +49,18 @@ brokerUdpListener.on('listening', function () {
 });
 
 var biotzData = {};
+var biotzCal = {};
+// received an update message - store info
 brokerUdpListener.on('message', function (message, remote) {
     if (message.length > 0)
     {
-        biotzData = JSON.parse(message);
-        console.log('bd', biotzData);
+        var jResponse = JSON.parse(message);
+        if (jResponse['t'] == 'dat')
+            biotzData = jResponse;
+        else if (jResponse['t'] == 'cal')
+            biotzCal = jResponse;
+        else
+            console.log("unknown response type");
     }
 });
 
@@ -89,17 +108,18 @@ function getRoot(req, res, next) {
         "   http://" + BROKER_HOST + ":" + BROKER_HTTP_PORT + "/biotz/synchronise\n" +
         "   http://" + BROKER_HOST + ":" + BROKER_HTTP_PORT + "/biotz/addresses\n" +
         "   http://" + BROKER_HOST + ":" + BROKER_HTTP_PORT + "/biotz/addresses/XXXXXX\n" +
+        "   http://" + BROKER_HOST + ":" + BROKER_HTTP_PORT + "/biotz/addresses/XXXXXX/calibration\n" +
         "   http://" + BROKER_HOST + ":" + BROKER_HTTP_PORT + "/biotz/addresses/XXXXXX/identify\n" +
+        "   http://" + BROKER_HOST + ":" + BROKER_HTTP_PORT + "/biotz/addresses/XXXXXX/data\n" +
         "   http://" + BROKER_HOST + ":" + BROKER_HTTP_PORT + "/biotz/addresses/XXXXXX/w\n" +
         "   http://" + BROKER_HOST + ":" + BROKER_HTTP_PORT + "/biotz/addresses/XXXXXX/x\n" +
         "   http://" + BROKER_HOST + ":" + BROKER_HTTP_PORT + "/biotz/addresses/XXXXXX/y\n" +
-        "   http://" + BROKER_HOST + ":" + BROKER_HTTP_PORT + "/biotz/addresses/XXXXXX/z\n" +
-        "   http://" + BROKER_HOST + ":" + BROKER_HTTP_PORT + "/biotz/addresses/XXXXXX/calibration\n" 
+        "   http://" + BROKER_HOST + ":" + BROKER_HTTP_PORT + "/biotz/addresses/XXXXXX/z\n" 
     );
     next();
 }
 
-function getBiotData(req, res, next) {
+function getAllBiotData(req, res, next) {
     sendBiotzRouterMessage();
     res.header("Access-Control-Allow-Origin", "*"); 
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -109,7 +129,7 @@ function getBiotData(req, res, next) {
     next();
 }
 
-function getBiot(req, res, next) {
+function getBiotData(req, res, next) {
 
     var address = req.params['address'];
     var nodes = biotzData.n;
@@ -121,6 +141,7 @@ function getBiot(req, res, next) {
             break;
         }
     }
+    // trigger update request for next time
     sendBiotzRouterMessage();
     res.header("Access-Control-Allow-Origin", "*"); 
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -131,22 +152,22 @@ function getBiot(req, res, next) {
 
 function getBiotCalibration(req, res, next) {
 
+    var address = req.params['address'];
+    var nodes = biotzCal.n;
+    var value = "";
+    for (var i = 0; i < nodes.length; i++) {
+        var node = nodes[i];
+        if (node.a === address) {
+            value = node.v;
+            break;
+        }
+    }
+    // trigger update request for next time
+    sendBiotzRouterMessage();
     res.header("Access-Control-Allow-Origin", "*"); 
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.setHeader('Content-Type', 'application/json');
-
-    var address = req.params['address'];
-    var message = new Buffer('get-calib:' + address);
-    var client = dgram.createSocket('udp6');
-
-    client.send(message, 0, message.length, BIOTZ_UDP_PORT, BIOTZ_ROUTER_HOST, function(err, bytes) {
-        if (err)
-        {
-            console.log('Error:', err);
-        }
-        client.close();
-    });
-    res.send('OK');
+    res.send(value);
     next();
 }
 
@@ -161,10 +182,58 @@ function getBiotCount(req, res, next) {
     next();
 }
 
+function getBiotFull(req, res, next) {
+
+    var address = req.params['address'];
+    if (! address) {
+        // url entered with trailing slash
+        getBiotz(req, res, next);
+        return;
+    }
+    var data = "";
+    var nodes = biotzData.n;
+    for (var i = 0; i < nodes.length; i++) {
+        var node = nodes[i];
+        if (node.a === address) {
+            data = node.v;
+            break;
+        }
+    }
+
+    var cal = "";
+    var nodes = biotzCal.n;
+    for (var i = 0; i < nodes.length; i++) {
+        var node = nodes[i];
+        if (node.a === address) {
+            cal = node.v;
+            break;
+        }
+    }
+    var value = {
+        'data': data,
+        'calibration': cal,
+    }
+
+
+    // trigger update request for next time
+    sendBiotzRouterMessage();
+    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.setHeader('Content-Type', 'application/json');
+    res.send(value);
+    next();
+}
+
+
 function getBiotQuality(req, res, next) {
 
     var address = req.params['address'];
     var quality = req.params['quality'];
+    if (! quality) {
+        // url entered with trailing slash
+        getBiotFull(req, res, next);
+        return;
+    }
     var nodes = biotzData.n;
     var value = "";
     for (var i = 0; i < nodes.length; i++) {
@@ -215,11 +284,13 @@ function getBiotz(req, res, next) {
 
 var brokerListener = restify.createServer();
 brokerListener.get('/', getRoot);
-brokerListener.get('/biotz', getBiotData);
+brokerListener.get('/biotz', getAllBiotData);
 brokerListener.get('/biotz/count', getBiotCount);
 brokerListener.get('/biotz/synchronise', biotSync);
 brokerListener.get('/biotz/addresses', getBiotz);
-brokerListener.get('/biotz/addresses/:address', getBiot);
+brokerListener.get('/biotz/addresses/:address', getBiotFull);
+brokerListener.get('/biotz/addresses/:address', getBiotFull);
+brokerListener.get('/biotz/addresses/:address/data', getBiotData);
 brokerListener.get('/biotz/addresses/:address/identify', biotIdentify);
 brokerListener.get('/biotz/addresses/:address/calibration', getBiotCalibration);
 brokerListener.get('/biotz/addresses/:address/:quality', getBiotQuality);
