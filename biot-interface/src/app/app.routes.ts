2ef57b379dfcb0fd2323280316b1c95d55d0c6c4
Jonathan Kelly <jonk@bigpond.com>
2016-09-09 14:26:51 +1000

    Started to add node assembly user interface.

--- biot-interface/src/app/app.routes.ts
+++ biot-interface/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { provideRouter, RouterConfig } from '@angular/router';
 
 import {About} from './components/about/about';
 import {Nodes} from './components/nodes/nodes';
+import {Assemblies} from './components/assemblies/assemblies';
 import {RepoBrowser} from './components/repo-browser/repo-browser';
 import {RepoList} from './components/repo-list/repo-list';
 import {RepoDetail} from './components/repo-detail/repo-detail';
@@ -9,6 +10,7 @@ import {RepoDetail} from './components/repo-detail/repo-detail';
 const routes: RouterConfig = [
   { path: '', redirectTo: 'nodes', terminal: true },
   { path: 'nodes', component: Nodes },
+  { path: 'assemblies', component: Assemblies },
   { path: 'about', component: About },
   { path: 'github', component: RepoBrowser, children: [
     { path: ':org', component: RepoList, children: [
