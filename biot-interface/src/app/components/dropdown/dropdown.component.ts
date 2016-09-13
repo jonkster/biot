2c2960f2f6ee76c020da18053f7e22d5f9170fee
Jonathan Kelly <jonk@bigpond.com>
2016-09-13 12:37:16 +1000

    Adding simple dropdown UI control.

---
+++ biot-interface/src/app/components/dropdown/dropdown.component.ts
@@ -0,0 +1,47 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+@Component({
+    selector: 'myDropdown',
+    inputs: ['items', 'itemColours', 'emptyValue'],
+    styleUrls: ['./dropdown.component.css' ],
+    template: `
+        <button class="dropdown_selector" (click)="toggleDropped()" >
+            <span><span [style.background]="getItemColour(selected)">&nbsp; &nbsp;</span> {{ selected }}</span><span class="down-arrow"></span>
+        </button>
+        <ul class="dropdowns" [hidden]="! isDropped" >
+            <li *ngFor="let item of items" (click)="selectItem(item)" [style.color]="getItemColour(item)"> {{ item }} </li>
+        </ul>
+        `
+})
+export class DropDownComponent {
+    items:Array<string>;
+    itemColours:Object;
+    emptyValue:string = 'select value...';
+    selected:string;
+    isDropped:boolean = false;
+    @Output() onValueSet = new EventEmitter<string>();
+
+    ngOnInit() {
+        this.selected = this.emptyValue;
+    }
+
+    colouriser(address): string { return 'ff0000'; };
+
+    getItemColour(item): string {
+        var colour = this.itemColours[item];
+        if (colour != undefined)
+            return '#' + colour;
+        else
+            return '#eee';
+    }
+
+    selectItem(val) {
+        this.selected = val;
+        this.isDropped = false;
+        this.onValueSet.emit(val);
+        console.log(val);
+    }
+
+    toggleDropped() {
+        this.isDropped = ! this.isDropped;
+    }
+}
